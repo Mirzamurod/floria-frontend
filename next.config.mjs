d0ea4fb0a4c2dbddee51f109141f0918865fcdc5
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2025-03-02 13:52:30 +0500

    changed config

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'utfs.io', pathname: '**' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com', pathname: '**' },
       { protocol: 'http', hostname: 'localhost', pathname: '/images/**' },
-      { protocol: 'https', hostname: 'floria.uz', pathname: '/images**' },
+      { protocol: 'https', hostname: 'floria.uz', pathname: '/images/**' },
     ],
   },
 }
