5f7316d2f3d859f17ba988d240b9c1d4a814d448
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2025-03-07 22:19:00 +0500

    test repayment

--- components/providers/layout.provider.tsx
+++ components/providers/layout.provider.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { FC, ReactNode } from 'react'
+import { useSession } from 'next-auth/react'
 import { useAppSelector } from '@/store'
 import { SidebarTrigger, useSidebar } from '../ui/sidebar'
 import Sidebar from '../sidebar'
 import { ModeToggle } from '../shared/mode-toggle'
 import { Button } from '../ui/button'
 import { cn } from '@/lib/utils'
+import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
+import { OctagonAlert } from 'lucide-react'
+import Link from 'next/link'
 
 interface IProps {
   children: ReactNode
@@ -15,6 +19,7 @@ interface IProps {
 const LayoutProvider: FC<IProps> = props => {
   const { children } = props
   const { open, setOpen } = useSidebar()
+  const { data: session } = useSession()
 
   const { sidebar } = useAppSelector(state => state.login)
 
@@ -23,16 +28,38 @@ const LayoutProvider: FC<IProps> = props => {
       {sidebar ? <Sidebar /> : null}
       <main className='w-full'>
         {sidebar ? (
-          <div className='border-b flex justify-between p-2 sidebar-width fixed bg-background z-50'>
-            <Button asChild size='icon' variant='outline' onClick={() => setOpen(!open)}>
-              <SidebarTrigger />
-            </Button>
-            <div>
-              <ModeToggle />
+          <>
+            <div className='border-b flex justify-between p-2 sidebar-width fixed bg-background z-50'>
+              <Button asChild size='icon' variant='outline' onClick={() => setOpen(!open)}>
+                <SidebarTrigger />
+              </Button>
+              <div>
+                <ModeToggle />
+              </div>
             </div>
-          </div>
+          </>
         ) : null}
-        <div className={cn('z-0', sidebar ? 'p-2 mt-14' : null)}>{children}</div>
+        <div className={cn('z-0', sidebar ? 'p-2 mt-14' : null)}>
+          {/* {!session?.currentUser?.card_name ||
+          !session?.currentUser?.card_number ||
+          !session?.currentUser?.telegramToken ||
+          !session?.currentUser?.userName ||
+          !session?.currentUser?.userPhone ? ( */}
+          <Alert className='mb-2'>
+            <OctagonAlert className='h-4 w-4' />
+            <AlertTitle>Diqqat!</AlertTitle>
+            <AlertDescription>
+              Profilega o'tib kerakli ma'lumotlarni kiritishingiz kerak, shunda telegram bot
+              ishlashni boshlaydi. Profilega o'tish{' '}
+              <Link href='/profile' className='underline'>
+                shu yerga
+              </Link>{' '}
+              bosing.
+            </AlertDescription>
+          </Alert>
+          {/* ) : null} */}
+          {children}
+        </div>
       </main>
     </>
   )
